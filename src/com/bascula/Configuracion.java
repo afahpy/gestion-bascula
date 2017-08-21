1d1c2cc64f92127309d5c1385d0789ed195ef504
miguelgenez <mgenezzeneo@gmail.com>
2017-08-21 10:18:28 -0400

    Tipos a utilizar

--- src/com/bascula/Configuracion.java
+++ src/com/bascula/Configuracion.java
@@ -17,10 +17,38 @@ public class Configuracion extends Config {
 
 	/************************* TIPO TIPOS *****************************/
 
+	public static final String TIPO_TIPO_MOVIMIENTOS = "Tipos de Movimiento";
+	public static final String TIPO_TIPO_OBJETOS = "Tipos de objeto";
+
+	
 	/*************************** TIPOS ********************************/
 
-	/*********************** Alias Permisos **************************/
+	//Tipos movimiento
+	public static final String TIPO_MOVIMIENTO_ENTRADA = "Entrada";
+	public static final String SIGLA_TIPO_MOVIMIENTO_ENTRADA = "TM-EN";
+	public static final String TIPO_MOVIMIENTO_SALIDA = "Salida";
+	public static final String SIGLA_TIPO_MOVIMIENTO_SALIDA = "TM-SA";
 	
+	//Tipos objeto
+	public static final String TIPO_OBJETO_ORIGEN_LUGAR = "Origen Lugar";
+	public static final String SIGLA_TIPO_OBJETO_ORIGEN_LUGAR = "TO-OR-LU";
+	public static final String TIPO_OBJETO_DESTINO_LUGAR = "Destino Lugar";
+	public static final String SIGLA_TIPO_OBJETO_DESTINO_LUGAR = "TO-DE-LU";
+	public static final String TIPO_OBJETO_CHAPA = "Chapa";
+	public static final String SIGLA_TIPO_OBJETO_CHAPA = "TO-CH";
+	public static final String TIPO_OBJETO_CHAPA_CARRETA = "Chapa Carreta";
+	public static final String SIGLA_TIPO_OBJETO_CHAPA_CARRETA = "TO-CH-CA";
+	public static final String TIPO_OBJETO_CHOFER = "Chofer";
+	public static final String SIGLA_TIPO_OBJETO_CHOFER = "TO-CHO";
+	public static final String TIPO_OBJETO_TRANSPORTADORA = "Transportadora";
+	public static final String SIGLA_TIPO_OBJETO_TRANSPORTADORA = "TO-TRANS";
+	public static final String TIPO_OBJETO_DESPACHANTE = "Despachante";
+	public static final String SIGLA_TIPO_OBJETO_DESPACHANTE = "TO-DESPA";
+
+	
+
+	/*********************** Alias Permisos **************************/
+
 	public static final String ALIAS_PERM_EDITAR_DATOS_PERSONALES = "datos_personales";
 
 	/*********************** Sistema Propiedad ************************/
--- src/com/bascula/util/population/DBPopulationTipos.java
+++ src/com/bascula/util/population/DBPopulationTipos.java
@@ -15,13 +15,56 @@ public class DBPopulationTipos {
 		rr.saveObject(d, "sys");
 	}
 
-	public void cargaTipos() throws Exception {
+	public void cargaTiposConTipoTipo(String descripcionTipoTipo, String[][] tipos) throws Exception {
+
+		TipoTipo tipoTipo = new TipoTipo();
+		tipoTipo.setDescripcion(descripcionTipoTipo);
+		this.grabarDB(tipoTipo);
+		System.out.println("--> Se cargo el TipoTipo: " + descripcionTipoTipo);
+
+		for (int i = 0; i < tipos.length; i++) {
+
+			Tipo tipo = new Tipo();
+			tipo.setDescripcion(tipos[i][0]);// Para la descripcion pos 1
+			tipo.setSigla(tipos[i][1]);// Para la sigla pos 2
+			tipo.setTipoTipo(tipoTipo);
+			this.grabarDB(tipo);
+			System.out.println("--> Se cargo el tipo: " + tipos[i][0]);
+
+		}
+
+	}
+
+	public void poblarTipos() throws Exception {
+
+		// Tipos de Movimiento
+		String descTipoMovimiento = Configuracion.TIPO_TIPO_MOVIMIENTOS;
+		String[][] tipoMovimientos = {
+				{ Configuracion.TIPO_MOVIMIENTO_ENTRADA, Configuracion.SIGLA_TIPO_MOVIMIENTO_ENTRADA },
+				{ Configuracion.TIPO_MOVIMIENTO_SALIDA, Configuracion.SIGLA_TIPO_MOVIMIENTO_SALIDA } };
+
+		this.cargaTiposConTipoTipo(descTipoMovimiento, tipoMovimientos);
+
+		// Tipos de Objeto
+
+		String descTipoObjeto = Configuracion.TIPO_TIPO_OBJETOS;
+		String[][] tiposObjeto = {
+				{ Configuracion.TIPO_OBJETO_ORIGEN_LUGAR, Configuracion.SIGLA_TIPO_OBJETO_ORIGEN_LUGAR },
+				{ Configuracion.TIPO_OBJETO_DESTINO_LUGAR, Configuracion.SIGLA_TIPO_OBJETO_DESTINO_LUGAR },
+				{ Configuracion.TIPO_OBJETO_CHAPA, Configuracion.SIGLA_TIPO_OBJETO_CHAPA },
+				{ Configuracion.TIPO_OBJETO_CHAPA_CARRETA, Configuracion.SIGLA_TIPO_OBJETO_CHAPA_CARRETA },
+				{ Configuracion.TIPO_OBJETO_CHOFER, Configuracion.SIGLA_TIPO_OBJETO_CHOFER },
+				{ Configuracion.TIPO_OBJETO_TRANSPORTADORA, Configuracion.SIGLA_TIPO_OBJETO_TRANSPORTADORA },
+				{ Configuracion.TIPO_OBJETO_DESPACHANTE, Configuracion.SIGLA_TIPO_OBJETO_DESPACHANTE } };
+
+		this.cargaTiposConTipoTipo(descTipoObjeto, tiposObjeto);
 
 	}
 
 	public static void main(String[] args) throws Exception {
 		DBPopulationTipos db = new DBPopulationTipos();
-		db.cargaTipos();
+		db.poblarTipos();
+
 	}
 
 }
