1161bd36823dcb57695e68b80436ebc4292f6756
miguelgenez <mgenezzeneo@gmail.com>
2017-08-21 12:14:24 -0400

    Eliminar de la lista, agregar nuevo, limpiar filtro, filtrar

--- src/com/bascula/gestion/ListaMovimientosViewModel.java
+++ src/com/bascula/gestion/ListaMovimientosViewModel.java
@@ -8,11 +8,12 @@ import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 
 import com.bascula.Configuracion;
 import com.bascula.GenericViewModelApp;
 import com.bascula.domain.MovimientoEntradaSalida;
-import com.logia.domain.RegisterDomain;
+import com.bascula.domain.RegisterDomain;
 
 public class ListaMovimientosViewModel extends GenericViewModelApp {
 
@@ -65,6 +66,55 @@ public class ListaMovimientosViewModel extends GenericViewModelApp {
 		params.put("modoFormulario", Configuracion.MODO_FORMULARIO_EDICION);
 		this.saltoDePagina(Configuracion.URL_MOVIMIENTO_EDIT, params);
 
+	}
+
+	@Command
+	public void crearMovimiento() {
+		Hashtable<String, Object> params = new Hashtable<String, Object>();
+		params.put("movimiento", new MovimientoEntradaSalida());
+		params.put("modoFormulario", Configuracion.MODO_FORMULARIO_EDICION);
+		this.saltoDePagina(Configuracion.URL_MOVIMIENTO_EDIT, params);
+
+	}
+
+	@Command
+	@NotifyChange("*")
+	public void eliminarMovimientos() throws Exception {
+
+		RegisterDomain rr = RegisterDomain.getInstance();
+
+		if (this.selectedMovimientos.size() <= 0) {
+			this.mensajeError("Debe seleccionar uno o mas elementos.");
+			return;
+		}
+
+		if (this.mensajeSiNo("Esta seguro que desea eliminar los elementos seleccionados?")) {
+			for (MovimientoEntradaSalida mov : this.selectedMovimientos) {
+				rr.deleteObject(mov);
+			}
+
+			this.mensajeInfo("Se han eliminado los elementos seleccionados");
+			this.selectedMovimientos = new ArrayList<MovimientoEntradaSalida>();
+			this.filtrar();
+		}
+
+	}
+
+	@Command
+	@NotifyChange("deudas")
+	public void filtrar() throws Exception {
+
+		RegisterDomain rr = RegisterDomain.getInstance();
+		List<MovimientoEntradaSalida> movimientos = new ArrayList<MovimientoEntradaSalida>();
+		movimientos = rr.getMovimientos();
+		this.movimientos = movimientos;
+
+	}
+
+	@Command
+	@NotifyChange("*")
+	public void limpiarFiltros() {
+		this.movimientos = new ArrayList<MovimientoEntradaSalida>();
 	}
 
 }
