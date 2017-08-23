722372f523e3e290189e0fec7e29ac42b69b4a26
gabyburgoz <anagabburgos14@gmail.com>
2017-08-23 15:01:13 -0400

    Reporte Movimiento

---
+++ src/com/bascula/gestion/reportes/ReporteMovimiento.java
@@ -0,0 +1,105 @@
+package com.bascula.gestion.reportes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.dynamicreports.report.builder.component.ComponentBuilder;
+import net.sf.dynamicreports.report.builder.component.VerticalListBuilder;
+
+import com.bascula.util.population.reporte.BasculaReporte;
+
+public class ReporteMovimiento extends BasculaReporte {
+	
+	private String filtro = ""; 
+
+	String[][] cols = { { "", WIDTH + "65" },
+			{ "", WIDTH + "65" },
+			{ "", WIDTH + "65" },
+			{ "", WIDTH + "65" },
+			{ "", WIDTH + "65" },
+			{ "", WIDTH + "65" },
+			{ "", WIDTH + "65" },};
+	
+	
+	
+	@Override
+	public void informacionReporte() {
+		this.setTitulo("Movimientos");
+		
+		VerticalListBuilder out = cmp.verticalList();
+		for (int i = 0; i < 1; i++) {
+			out.add(cabecera());
+			out.add(datosDetalle());
+		}
+		
+		this.setBody(out);
+	}
+
+		
+	
+private ComponentBuilder cabecera(){
+	
+	VerticalListBuilder out = cmp.verticalList();
+	 VerticalListBuilder filtro = cmp.verticalList();
+	 filtro.add(this.textoParValor("Filtro", ""));
+
+	 
+	 out.add(filtro);
+	
+	return out;
+}
+
+	
+	private ComponentBuilder datosDetalle()  {
+		VerticalListBuilder out = cmp.verticalList();
+		
+		
+		// tabla Movimiento
+		
+				List<Object[]> listaMovimientos = this.getMovimientos();
+				out.add(this.espacioAlto(20));
+				String tablaMovim = LETRA_8 + TABLA_TITULO + "";
+
+			
+				ComponentBuilder mov = this.getTabla(cols, listaMovimientos,
+						tablaMovim, true, false, false);
+				out.add(mov);
+				out.add(this.espacioAlto(20));
+		return out;
+		
+		
+		
+	
+	}
+	
+	public List<Object[]> getMovimientos()  {
+		List<Object[]>movimientos= new ArrayList<Object[]>();
+		return movimientos ;
+	
+	}
+	
+	public static void main(String[] args) {
+		ReporteMovimiento reporte = new ReporteMovimiento();
+		
+		reporte.ejecutar(true);
+	}
+
+
+
+
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+
+
+
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+	
+	
+	
+}
