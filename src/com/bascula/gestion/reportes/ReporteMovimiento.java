89192a4a1159c9ac1876f530860a34803e4c3250
gabyburgoz <anagabburgos14@gmail.com>
2017-08-24 11:18:50 -0400

    Correcciones reporte movimiento

--- src/com/bascula/gestion/reportes/ReporteMovimiento.java
+++ src/com/bascula/gestion/reportes/ReporteMovimiento.java
@@ -9,110 +9,106 @@ import net.sf.dynamicreports.report.builder.component.VerticalListBuilder;
 import com.bascula.util.population.reporte.BasculaReporte;
 
 public class ReporteMovimiento extends BasculaReporte {
-	
-	private String filtro = ""; 
 
-	String[][] cols = { { "", WIDTH + "65" },
-			{ "", WIDTH + "65" },
-			{ "", WIDTH + "65" },
-			{ "", WIDTH + "65" },
-			{ "", WIDTH + "65" },
-			{ "", WIDTH + "65" },
-			{ "", WIDTH + "65" },};
-	
-	
-	
+	private String filtro = "";
+
+	String[][] cols = null;
+	List<Object[]> listaMovimiento = null;
+
+	public List<Object[]> getListaMovimiento() {
+		return listaMovimiento;
+	}
+
+	public void setListaMovimiento(List<Object[]> listaMovimiento) {
+		this.listaMovimiento = listaMovimiento;
+	}
+
 	@Override
 	public void informacionReporte() {
 		this.setTitulo("Movimientos");
-		
+
 		VerticalListBuilder out = cmp.verticalList();
 		for (int i = 0; i < 1; i++) {
 			out.add(cabecera());
 			out.add(datosDetalle());
 		}
-		
+
 		this.setBody(out);
 	}
 
-		
-	
-private ComponentBuilder cabecera(){
-	
-	VerticalListBuilder out = cmp.verticalList();
-	 VerticalListBuilder filtro = cmp.verticalList();
-	 filtro.add(this.textoParValor("Filtro", this.filtro));
+	private ComponentBuilder cabecera() {
 
-	 
-	 out.add(filtro);
-	
-	return out;
-}
-
-	
-	private ComponentBuilder datosDetalle()  {
 		VerticalListBuilder out = cmp.verticalList();
-		
-		
-		// tabla Movimiento
-		
-				List<Object[]> listaMovimientos = this.getMovimientos();
-				out.add(this.espacioAlto(20));
-				String tablaMovim = LETRA_8 + TABLA_TITULO + "";
+		VerticalListBuilder filtro = cmp.verticalList();
+		filtro.add(this.textoParValor("Filtro", this.filtro));
 
-			
-				ComponentBuilder mov = this.getTabla(cols, listaMovimientos,
-						tablaMovim, true, false, false);
-				out.add(mov);
-				out.add(this.espacioAlto(20));
+		out.add(filtro);
+
 		return out;
-		
-		
-		
-	
-	}
-	
-	public List<Object[]> getMovimientos()  {
-		List<Object[]>movimientos = new ArrayList<Object[]>();;
-		
-		
-		for (int i = 0; i < 5; i++) {
-			
-			Object[] fila = new String[7];
-			for (int j = 0; j < 7; j++) {
-				fila[j] = "i:"+i+" j:"+j;
-			}
-			movimientos.add(fila);
-		}
-			
-		
-		return movimientos ;
-	
-	}
-	
-	public static void main(String[] args) {
-		ReporteMovimiento reporte = new ReporteMovimiento();
-		
-		reporte.setFiltro("123");
-		reporte.ejecutar(true);
 	}
 
+	private ComponentBuilder datosDetalle() {
+		VerticalListBuilder out = cmp.verticalList();
 
+		out.add(this.espacioAlto(20));
+		String tablaMovim = LETRA_8 + TABLA_TITULO + "";
 
+		ComponentBuilder mov = this.getTabla(this.cols, this.listaMovimiento,
+				tablaMovim, true, false, false);
+		out.add(mov);
+		out.add(this.espacioAlto(20));
+		return out;
 
+	}
+
+	public String[][] getCols() {
+		return cols;
+	}
+
+	public void setCols(String[][] cols) {
+		this.cols = cols;
+	}
 
 	public String getFiltro() {
 		return filtro;
 	}
 
-
-
-
-
 	public void setFiltro(String filtro) {
 		this.filtro = filtro;
 	}
 	
-	
-	
+	//================================================================
+	public static List<Object[]> getMovimientosPrueba(int nCol) {
+		List<Object[]> movimientos = new ArrayList<Object[]>();
+		;
+
+		for (int i = 0; i < nCol; i++) {
+
+			Object[] fila = new String[7];
+			for (int j = 0; j < 7; j++) {
+				fila[j] = "fil:" + i + " col:" + j;
+			}
+			movimientos.add(fila);
+		}
+
+		return movimientos;
+
+	}
+	//================================================================
+
+	public static void main(String[] args) {
+		ReporteMovimiento reporte = new ReporteMovimiento();
+
+		String[][] cols = { { "Col1", WIDTH + "65" }, { "Col2", WIDTH + "65" },
+				{ "Col3", WIDTH + "65" }, { "Col4", WIDTH + "65" },
+				{ "Col5", WIDTH + "65" }, { "Col6", WIDTH + "65" },
+				{ "Col7", WIDTH + "65" }, };
+
+		reporte.setFiltro("123");
+		reporte.setCols(cols);
+		reporte.setListaMovimiento(getMovimientosPrueba(cols.length));
+		reporte.ejecutar(true);
+	}
+
+
 }
